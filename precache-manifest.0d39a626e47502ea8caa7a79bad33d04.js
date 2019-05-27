c80a0ff3cec78298cf0327f95d95f00edcb0cd79
maxim_mariukhin <maryuhin.z@gmail.com>
2019-05-27 10:53:47 +0300

    Updates

---
+++ precache-manifest.0d39a626e47502ea8caa7a79bad33d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  },
+  {
+    "revision": "6f118129df7de5f23538",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.f0f48701.chunk.js"
+  },
+  {
+    "revision": "00cf36eb0fc1fd8a1506",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.ee347af6.chunk.js"
+  },
+  {
+    "revision": "6f118129df7de5f23538",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.f0e474f7.chunk.css"
+  },
+  {
+    "revision": "00cf36eb0fc1fd8a1506",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.ca4eeedc.chunk.css"
+  },
+  {
+    "revision": "ff60c6e16af3f2adb31c7e03764d9d7e",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  }
+];
